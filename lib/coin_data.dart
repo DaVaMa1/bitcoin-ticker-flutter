04764c96acc4534d7afa9f9adf2ac7c4b9375038
Daan van Marle <daanvanmarle@dsw.nl>
2019-05-27 13:08:31 +0200

    Almost finished gold medal, need some way to communicate data 2 ways.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,195 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future<CurrencyExchangeRate> getExchangeRate(String selectedCurrency) {
+    return http
+        .get(
+            'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC$selectedCurrency')
+        .then((response) {
+      return CurrencyExchangeRate.fromJson(json.decode(response.body));
+    });
+  }
+}
+
+class CurrencyExchangeRate {
+  double ask;
+  double bid;
+  double last;
+  double high;
+  double low;
+  Open open;
+  Averages averages;
+  double volume;
+  Changes changes;
+  double volumePercent;
+  int timestamp;
+  String displayTimestamp;
+
+  CurrencyExchangeRate(
+      {this.ask,
+      this.bid,
+      this.last,
+      this.high,
+      this.low,
+      this.open,
+      this.averages,
+      this.volume,
+      this.changes,
+      this.volumePercent,
+      this.timestamp,
+      this.displayTimestamp});
+
+  CurrencyExchangeRate.fromJson(Map<String, dynamic> json) {
+    ask = json['ask'];
+    bid = json['bid'];
+    last = json['last'];
+    high = json['high'];
+    low = json['low'];
+    open = json['open'] != null ? new Open.fromJson(json['open']) : null;
+    averages = json['averages'] != null
+        ? new Averages.fromJson(json['averages'])
+        : null;
+    volume = json['volume'];
+    changes =
+        json['changes'] != null ? new Changes.fromJson(json['changes']) : null;
+    volumePercent = json['volume_percent'];
+    timestamp = json['timestamp'];
+    displayTimestamp = json['display_timestamp'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['ask'] = this.ask;
+    data['bid'] = this.bid;
+    data['last'] = this.last;
+    data['high'] = this.high;
+    data['low'] = this.low;
+    if (this.open != null) {
+      data['open'] = this.open.toJson();
+    }
+    if (this.averages != null) {
+      data['averages'] = this.averages.toJson();
+    }
+    data['volume'] = this.volume;
+    if (this.changes != null) {
+      data['changes'] = this.changes.toJson();
+    }
+    data['volume_percent'] = this.volumePercent;
+    data['timestamp'] = this.timestamp;
+    data['display_timestamp'] = this.displayTimestamp;
+    return data;
+  }
+}
+
+class Open {
+  double day;
+  double week;
+  double month;
+
+  Open({this.day, this.week, this.month});
+
+  Open.fromJson(Map<String, dynamic> json) {
+    day = json['day'];
+    week = json['week'];
+    month = json['month'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['day'] = this.day;
+    data['week'] = this.week;
+    data['month'] = this.month;
+    return data;
+  }
+}
+
+class Averages {
+  double daily;
+  double weekly;
+  double monthly;
+
+  Averages({this.daily, this.weekly, this.monthly});
+
+  Averages.fromJson(Map<String, dynamic> json) {
+    daily = json['daily'];
+    weekly = json['weekly'];
+    monthly = json['monthly'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['daily'] = this.daily;
+    data['weekly'] = this.weekly;
+    data['monthly'] = this.monthly;
+    return data;
+  }
+}
+
+class Changes {
+  Price price;
+  Percent percent;
+
+  Changes({this.price, this.percent});
+
+  Changes.fromJson(Map<String, dynamic> json) {
+    price = json['price'] != null ? new Price.fromJson(json['price']) : null;
+    percent =
+        json['percent'] != null ? new Percent.fromJson(json['percent']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.price != null) {
+      data['price'] = this.price.toJson();
+    }
+    if (this.percent != null) {
+      data['percent'] = this.percent.toJson();
+    }
+    return data;
+  }
+}
+
+class Price {
+  double weekly;
+  double monthly;
+  double daily;
+
+  Price({this.weekly, this.monthly, this.daily});
+
+  Price.fromJson(Map<String, dynamic> json) {
+    weekly = json['weekly'];
+    monthly = json['monthly'];
+    daily = json['daily'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['weekly'] = this.weekly;
+    data['monthly'] = this.monthly;
+    data['daily'] = this.daily;
+    return data;
+  }
+}
+
+class Percent {
+  double weekly;
+  double monthly;
+  double daily;
+
+  Percent({this.weekly, this.monthly, this.daily});
+
+  Percent.fromJson(Map<String, dynamic> json) {
+    weekly = json['weekly'];
+    monthly = json['monthly'];
+    daily = json['daily'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['weekly'] = this.weekly;
+    data['monthly'] = this.monthly;
+    data['daily'] = this.daily;
+    return data;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +10,32 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = currenciesList.first;
+  List<Widget> cryptos = List<Widget>();
+
+  @override
+  initState() {
+    super.initState();
+    setState(() {
+      cryptos.addAll(
+        cryptoList.map((crypto) {
+          return CryptoExchangeRateDisplay(
+            crypto,
+            selectedCurrency: selectedCurrency,
+            selectedCurrencyExchangeRate: '?',
+          );
+        }),
+      );
+      cryptos.add(Container(
+        height: 150.0,
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(bottom: 30.0),
+        color: Colors.lightBlue,
+        child: Platform.isAndroid ? getDropDown() : getPicker(),
+      ));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,36 +45,86 @@ class _PriceScreenState extends State<PriceScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+        children: cryptos,
+      ),
+    );
+  }
+
+  CupertinoPicker getPicker() {
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (index) {
+        setState(() {
+          selectedCurrency = currenciesList[index];
+        });
+      },
+      children: currenciesList.map((f) => Text(f)).toList(),
+    );
+  }
+
+  DropdownButton<String> getDropDown() {
+    return DropdownButton(
+        value: selectedCurrency,
+        items: currenciesList
+            .map((f) => DropdownMenuItem(
+                  child: Text(f),
+                  value: f,
+                ))
+            .toList(),
+        onChanged: (value) {
+          setState(() {
+            selectedCurrency = value;
+          });
+        });
+  }
+}
+
+class CryptoExchangeRateDisplay extends StatefulWidget {
+  CryptoExchangeRateDisplay(
+    this.crypto, {
+    this.selectedCurrencyExchangeRate,
+    this.selectedCurrency,
+  });
+
+  final String selectedCurrencyExchangeRate;
+  final String selectedCurrency;
+  final String crypto;
+
+  @override
+  _CryptoExchangeRateDisplayState createState() =>
+      _CryptoExchangeRateDisplayState(
+          selectedCurrencyExchangeRate, selectedCurrency);
+}
+
+class _CryptoExchangeRateDisplayState extends State<CryptoExchangeRateDisplay> {
+  String selectedCurrencyExchangeRate;
+  String selectedCurrency;
+
+  _CryptoExchangeRateDisplayState(
+      this.selectedCurrencyExchangeRate, this.selectedCurrency);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '${widget.crypto} = $selectedCurrencyExchangeRate $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
             ),
           ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
-        ],
+        ),
       ),
     );
   }
